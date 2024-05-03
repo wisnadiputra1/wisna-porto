42cbed76ae52afdfa0cddcea9a4e9678125bad76
wisnadiputra1 <wisnadiputra@gmail.com>
2024-05-03 15:28:17 +0800

    Adjusment responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,15 +26,15 @@ const Contact = () => {
   ]
   
   return (
-    <div className='font-Kanit mt-16 flex flex-col gap-20'>
+    <div className='font-Kanit mt-16 flex flex-col gap-20 md:grid md grid-cols-2'>
       <div className='space-y-16'>
         <div className='space-y-6'>
             <h2 className='text-5xl font-bold'>{"Let's Keep Connect"}</h2>
-            <p className='text-text-main'>{"I'm always open to new opportunities and collaborations. If you have a project in mind or want to discuss how I can contribute to your team, please don't hesitate to reach out. You can find my contact information on this site."}</p>
+            <p className='text-text-main md:text-2xl w-full'>{"I'm always open to new opportunities and collaborations. If you have a project in mind or want to discuss how I can contribute to your team, please don't hesitate to reach out. You can find my contact information on this site."}</p>
         </div>
 
         <div className='w-ful flex items-center'>
-          <ul className='w-full flex flex-wrap gap-20 justify-center items-center'>
+          <ul className='w-full md:w-[90%] flex flex-wrap gap-x-28 gap-y-8 justify-center items-center md:justify-start'>
             {
               listSocialMedia.length > 0 ? listSocialMedia.map((item) => {
               return <div key={item.text}>
@@ -47,23 +47,23 @@ const Contact = () => {
           </ul>
         </div>
 
-        <div className='flex flex-col items-center gap-6'>
-        <h3 className='text-5xl font-bold'>Get In Touch With Us</h3>
-        <p className='text-text-main'>Feel free to contact us for collaboration opportunities or freelance work. We're open to exciting projects and look forward to hearing from you!</p>
-      </div>
+        <div className='flex flex-col gap-6'>
+          <h3 className='text-5xl font-bold md:text-5xl'>Get In Touch With Us</h3>
+          <p className='text-text-main md:text-2xl'>Feel free to contact us for collaboration opportunities or freelance work. We're open to exciting projects and look forward to hearing from you!</p>
+        </div>
       </div>
 
       <div className='space-y-10'>
         <div>
-          <img src={vector} alt="" className='w-[250px' />
+          <img src={vector} alt="Vector Email" className='w-[250px md:w-full' />
         </div>
         <div>
           <div className='bg-secondary rounded-md'>
-            <form className='py-1 flex flex-col items-center justify-center gap-3'>
+            <form className='py-1 flex flex-col items-center justify-center gap-3 mx-3'>
               <h2 className='font-semibold'>Send An Email</h2>
-                <input type="text" placeholder='Name' className='rounded-md px-2 w-[330px] py-2 focus:outline-sky-600' />
-                <input type="email" placeholder='E-mail' className='rounded-md px-2 w-[330px] py-2' />
-                <textarea name="Message" id="msg" placeholder='Type message here..' className='rounded-md px-2 py-2 w-[330px] h-36'></textarea>
+                <input type="text" placeholder='Name' className='rounded-md px-2 w-full py-2 focus:outline-sky-600' />
+                <input type="email" placeholder='E-mail' className='rounded-md px-2 w-full py-2' />
+                <textarea name="Message" id="msg" placeholder='Type message here..' className='rounded-md px-2 py-2 w-full h-36'></textarea>
                 <button type='submit' className='bg-black text-white px-10 py-1 rounded-full'>Send</button>
             </form>
           </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
 
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-r from-secondary via-bg-secondary to-bg-third ... py-4 mt-10 font-Kanit ">
-        <div className="flex flex-col items-center justify-center mx-3 gap-6 ">
-            <div className="space-y-5">
-                <div className="flex flex-col items-center">
+    <div className="bg-gradient-to-r from-secondary via-bg-secondary to-bg-third ... py-4 md:py-10 mt-10 font-Kanit">
+        <div className="flex flex-col items-center justify-center md:justify-start mx-3 md:mx-[416px] gap-6 md:grid md:grid-cols-2">
+            <div className="space-y-5 md:space-y-0 md:flex md:gap-16 md:w-full">
+                <div className="flex flex-col items-center md:items-start">
                     <h2 className="font-bold">Portfolio</h2>
-                    <ul className="flex flex-col items-center">
+                    <ul className="flex flex-col items-center md:items-start">
                         <li>Home</li>
                         <li>Skills</li>
                         <li>Projects</li>
                         <li>Contact</li>
                     </ul>
                 </div>
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center md:items-start">
                     <h2 className="font-bold">Social Media</h2>
-                    <ul className="flex flex-col items-center">
+                    <ul className="flex flex-col items-center md:items-start">
                         <li>GIthub</li>
                         <li>LinkedIn</li>
                         <li>Gmail</li>
@@ -24,8 +24,8 @@ const Footer = () => {
                 </div>
             </div>
             
-            <div className="w-[300px]">
-                <h2 className="text-5xl font-bold">BE CREATIVE, BE SOLUTIVE</h2>
+            <div className="w-[300px] md:w-full md:flex md:flex-col md:justify-end md:items-end">
+                <h2 className="text-5xl md:text-7xl font-bold md:w-[450px]">BE CREATIVE, BE SOLUTIVE</h2>
             </div>
         </div>
     </div>
